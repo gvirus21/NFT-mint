0ff2bb06574948a4e6f7ab57142238ef69975024
gourav <gvirus21yt@gmail.com>
2022-05-06 16:36:58 +0530

    added image logo to header & changed background color of label

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Image from 'next/image';
+import logo from '../assets/pika.png'
 
 const style = {
   wrapper:
     "flex justify-between items-center px-20 h-20 w-screen absolute top-0 z-30 bg-gradient-to-r from-slate-700 via-gray-700 to-slate-900 ",
   logo: "font-semibold text-2xl",
-  accountLabel: "text-white bg-violet-300 px-6 py-2 rounded-full",
+  accountLabel: "text-white bg-pink-600 px-3 py-2 rounded-md",
   button:
     "bg-zinc-300 px-6 py-2 rounded-xl text-xl hover:bg-zinc-400 transition-colors active:bg-zinc-200	",
 };
@@ -12,8 +14,8 @@ const style = {
 const Navbar = ({isConnected, connectWallet, currentAccount}) => {
     return (
       <nav className={style.wrapper}>
-        <h1 className={style.logo}>PokeMint</h1>
-
+        {/* <h1 className={style.logo}>PokeMint</h1> */}
+        <Image src={logo} height='50px' width='70px' alt='logo'/>
         {isConnected ? (
           <p className={style.accountLabel}>{`${currentAccount.substr(
             0,
